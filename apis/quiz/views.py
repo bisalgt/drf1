5fb627fd9e042faf1aa2ad2814e2fd399259d373
Bishal Gautam <bisalgt@gmail.com>
2020-04-04 14:04:25 +0545

    added custom throttling decorators for home page

--- apis/quiz/views.py
+++ apis/quiz/views.py
@@ -1,13 +1,16 @@
 from rest_framework.decorators import api_view, throttle_classes
 from rest_framework.response import Response
-# from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 
-
+# custom throttling
+class CustomAnonymousRateThrottle(AnonRateThrottle):
+	rate='3/day'
 
 
 
 
 @api_view(['GET','POST'])
+@throttle_classes([CustomAnonymousRateThrottle,UserRateThrottle])
 def home(request):
 	print(dir(request))
 	if request.method=='GET':
